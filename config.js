a91e1af75000e19e9a4468a186b9e6e950cdcd92
shrey-creator <60284082+shrey-creator@users.noreply.github.com>
2022-03-13 11:52:52 +0530

    make it better

---
+++ config.js
@@ -0,0 +1,18 @@
+// const firebase=require("firebase");
+
+// const firebaseConfig = {
+//     apiKey: "AIzaSyBM7xINkwLw8bqA4qhJEXk2oMFcUxvugUU",
+//     authDomain: "interview-98384.firebaseapp.com",
+//     projectId: "interview-98384",
+//     storageBucket: "interview-98384.appspot.com",
+//     messagingSenderId: "1036332029731",
+//     appId: "1:1036332029731:web:c876c5a2c516ffdc06de96",
+//     measurementId: "G-SN3C9YD895"
+//   };
+
+// firebase.initialiseAoo(firebaseConfig);
+// const db=firebase.firestore();
+// const participants=db.collection("participantsSchema");
+// const interviewer=db.collection("interviewerSchema");
+// const interviewee=db.collection("intervieweeSchema");
+
--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 3000
-app.use(express.static('./client'));
+const path = require('path');
+
+app.use(express.static('client'));
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+// // var path=
+//   console.log();
+//    res.sendFile(path.join(__dirname, '../client/index.html'));
+// res.send("hello")
 });
 
 app.listen(process.env.PORT||3000, () => {
